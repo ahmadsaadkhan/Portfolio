08ed78da1258c66c977086daf3f7a045935ef650
ahmadsaadkhan <ahmad.saad.khan@live.com>
2021-09-03 12:51:15 +0300

    protfolio updated

---
+++ src/Components/Footer.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  const currentdate = new Date()
+  const datetime = currentdate.getFullYear()
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+            &copy; Ahmad Saad Khan {datetime}
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
